1d354903df26e22acdd189a2cbd9f59bc0157274
NDL <thang12cuaanhh@gmail.com>
2026-05-02 22:37:12 +0700

    Modify IF_ID register to include flush functionality
    Added flush input to reset logic and set instr_id to NOP.

--- src/IF_ID_reg.v
+++ src/IF_ID_reg.v
@@ -1,13 +1,14 @@
 module if_id_reg (
     input wire clk, rst,
+    input wire flush, 
     input wire [31:0] pc_if, pc_plus_4_if, instr_if,
     output reg [31:0] pc_id, pc_plus_4_id, instr_id
 );
     always @(posedge clk or posedge rst) begin
-        if (rst) begin
-            pc_id <= 32'b0;
+        if (rst || flush) begin
+            pc_id       <= 32'b0;
             pc_plus_4_id <= 32'b0;
-            instr_id <= 32'b0;
+            instr_id    <= 32'h0000_0013;   // NOP = addi x0,x0,0
         end else begin
             pc_id <= pc_if;
             pc_plus_4_id <= pc_plus_4_if;
